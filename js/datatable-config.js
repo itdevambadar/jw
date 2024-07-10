6a9c0ccca02e54beca42e4f79b8525147e028088
itdevambadar <it_support@ambadar.co.id>
2024-07-10 10:57:15 +0700

    update

--- js/datatable-config.js
+++ js/datatable-config.js
@@ -1094,5 +1094,37 @@ $(function(){
         ],
         'iDisplayLength': 20,
     });
+    $('#table37').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'copyHtml5',
+                text: '<i class="fa fa-files-o"></i>',
+                titleAttr: 'Copy'
+            },
+            {
+                extend: 'excelHtml5',
+                text: '<i class="fa fa-file-excel-o"></i>',
+                titleAttr: 'Excel'
+            },
+            {
+                extend: 'csvHtml5',
+                text: '<i class="fa fa-file-text-o"></i>',
+                titleAttr: 'CSV'
+            },
+            {
+                extend: 'pdfHtml5',
+                text: '<i class="fa fa-file-pdf-o"></i>',
+                titleAttr: 'PDF'
+            },
+            {
+                extend: 'print',
+                text: '<i class="fa fa-print"></i>',
+                titleAttr: 'Print'
+            }
+        ],
+        'iDisplayLength': 20,
+    });
+    
 });
 
